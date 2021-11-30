2dbb89ef56eb0e4051bb5f3fb69c7cec7a8676db
Tatyana Pronicheva <pronicheva40@gmail.com>
2021-11-30 10:42:32 +0300

    Added homework for lesson2 java2

---
+++ src/MyArrayDataException.java
@@ -0,0 +1,5 @@
+public class MyArrayDataException extends Exception{
+    public MyArrayDataException(String msg){
+        super(msg);
+    }
+}
---
+++ src/MyArraySizeException.java
@@ -0,0 +1,5 @@
+public class MyArraySizeException extends Exception{
+    public MyArraySizeException(){
+        super("Введен массив некорректного размера");
+    }
+}
